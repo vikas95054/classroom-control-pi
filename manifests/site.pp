31b52f08d956a588c7959b6cf96415fa459c9915
David Crouse <david@crouse.us>
2017-02-07 16:07:02 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,6 +31,6 @@ node default {
   
   notify { "This is the default message from the production environment": }
   # Lab 3.1
-  notify { "Hello, my name is Dave"; }
+  notify { "Hello world! I am ${::fqdn}": }
 }
 
